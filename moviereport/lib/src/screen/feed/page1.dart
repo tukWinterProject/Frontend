fb07d81b9911c82cc8649d02cf7f9ba96d1da623
HaeunKim01 <tobebeo382@naver.com>
2024-01-19 16:25:52 +0900

    로그인 회원가입 구성만

--- moviereport/lib/src/screen/feed/page1.dart
+++ moviereport/lib/src/screen/feed/page1.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:moviereport/src/screen/register/login.dart'; // 로그인 페이지 파일 임포트
 
 class Page1 extends StatelessWidget {
-  const Page1({super.key});
+  const Page1({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("page1"),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("page1"),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // '시작하기' 버튼이 눌렸을 때 로그인 페이지로 이동
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Login()),
+              );
+            },
+            child: const Text('지금 시작하기'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ moviereport/lib/src/screen/register/register.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:moviereport/src/screen/register/login.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  final RegExp emailRegex =
+      RegExp(r'^[a-zA-Z0-9_+.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final String id = _idController.text;
+      final String password = _passwordController.text;
+      final String name = _nameController.text;
+      // 회원가입 로직 추가
+      // 회원가입 성공 시, 다음 화면으로 이동 혹은 처리
+    }
+  }
+
+  void _goToLoginPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Login()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('회원가입')),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              controller: _idController,
+              decoration: const InputDecoration(
+                labelText: 'NICKNAME',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '닉네임을 입력하세요.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: 'ID',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '아이디를 입력하세요.';
+                }
+                // 추가적인 아이디 유효성 검사 로직 추가 가능
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'PASSWORD',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '비밀번호를 입력하세요.';
+                }
+                // 추가적인 비밀번호 유효성 검사 로직 추가 가능
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submitForm,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xffFF7E36),
+              ),
+              child: const Text(
+                '회원 가입',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('이미 회원이신가요?'),
+                TextButton(
+                  onPressed: _goToLoginPage,
+                  child: const Text(
+                    '로그인',
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
