3af07c913cd581c8a35b22ec1b30638885bfd6e9
HaeunKim01 <tobebeo382@naver.com>
2024-01-22 20:22:45 +0900

    로그인 회원가입 퍼블리싱 완료

--- moviereport/lib/src/screen/register/login.dart
+++ moviereport/lib/src/screen/register/login.dart
@@ -12,7 +12,6 @@ class _LoginState extends State<Login> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _nameController = TextEditingController();
 
   final RegExp emailRegex =
       RegExp(r'^[a-zA-Z0-9_+.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
@@ -36,75 +35,96 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Movie Report',
+          style:
+              TextStyle(color: Color(0xffFF3743), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFFF0F0F0),
+        elevation: 0,
+      ),
       body: Form(
         key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: [
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'ID',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '아이디를 입력하세요.';
-                }
-                if (!emailRegex.hasMatch(value)) {
-                  return '이메일 형식에 맞지 않습니다.';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'PASSWORD',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '비밀번호를 입력하세요.';
-                }
-                // 추가적인 비밀번호 유효성 검사 로직 추가 가능
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submitForm,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xffFF3743),
-              ),
-              child: const Text(
-                '로그인',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        child: Center(
+          child: ListView(
+            padding: const EdgeInsets.all(50.0),
+            children: [
+              SizedBox(height: 80),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'ID',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '아이디를 입력하세요.';
+                  }
+                  if (!emailRegex.hasMatch(value)) {
+                    return '이메일 형식에 맞지 않습니다.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('회원이 아니신가요?'),
-                TextButton(
-                  onPressed: _goToRegisterPage,
-                  child: const Text(
-                    '회원가입',
-                    style: TextStyle(
-                      color: Colors.red,
-                    ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'PASSWORD',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '비밀번호를 입력하세요.';
+                  }
+                  // 추가적인 비밀번호 유효성 검사 로직 추가 가능
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submitForm,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffFF3743),
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  '로그인',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('회원이 아니신가요?'),
+                  TextButton(
+                    onPressed: _goToRegisterPage,
+                    child: const Text(
+                      '회원가입',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- moviereport/lib/src/screen/register/register.dart
+++ moviereport/lib/src/screen/register/register.dart
@@ -37,88 +37,114 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: [
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'NICKNAME',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '닉네임을 입력하세요.';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'EMAIL',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '이메일을 입력하세요.';
-                }
-                if (!emailRegex.hasMatch(value)) {
-                  return '이메일 형식에 맞지 않습니다.';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'PASSWORD',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '비밀번호를 입력하세요.';
-                }
-                // 추가적인 비밀번호 유효성 검사 로직 추가 가능
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submitForm,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xffFF3743),
-              ),
-              child: const Text(
-                '회원 가입',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Movie Report',
+          style:
+              TextStyle(color: Color(0xffFF3743), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFFF0F0F0),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: const EdgeInsets.all(50.0),
+            children: [
+              SizedBox(height: 80),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'NICKNAME',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '닉네임을 입력하세요.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('이미 회원이신가요?'),
-                TextButton(
-                  onPressed: _goToLoginPage,
-                  child: const Text(
-                    '로그인',
-                    style: TextStyle(
-                      color: Color(0xffFF3743),
-                    ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'EMAIL',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '이메일을 입력하세요.';
+                  }
+                  if (!emailRegex.hasMatch(value)) {
+                    return '이메일 형식에 맞지 않습니다.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'PASSWORD',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '비밀번호를 입력하세요.';
+                  }
+                  // 추가적인 비밀번호 유효성 검사 로직 추가 가능
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _submitForm,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffFF3743),
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  '회원 가입',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('이미 회원이신가요?'),
+                  TextButton(
+                    onPressed: _goToLoginPage,
+                    child: const Text(
+                      '로그인',
+                      style: TextStyle(
+                        color: Color(0xffFF3743),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
